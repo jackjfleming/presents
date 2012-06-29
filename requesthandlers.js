6e78d110cc883b44b5d58b4cd13aa27df0da5125
libra guest <jackj.fleming@gmail.com>
2012-06-28 22:50:32 -0400

    Started

---
+++ requesthandlers.js
@@ -0,0 +1,73 @@
+var querystring = require("querystring");
+	fs = require("fs");
+
+function start(response, postData){
+	console.log("Request handler 'start' was called.");
+	
+	fs.readFile("./start.html",function(err,data){
+		if(err)
+			throw err;		
+		
+		response.writeHead(200,{"Content-Type":"text/html"});
+		response.write(data)
+		response.end();
+	});
+}
+
+function upload(response, postData){
+	console.log("Request handler 'upload' was called.");
+	response.writeHead(200,{"Content-Type": "text/plain"});
+	response.write("You've sent: " + querystring.parse(postData).text);
+	response.end();
+}
+
+function show(response, postData){
+	console.log("Request handler 'show' was called.");
+	fs.readFile("/Users/Jack/Node1/tmp/test.png","binary",function(error,file){
+		if(error){
+			response.writeHead(500,{"Content-Type": "text/plain"});
+			response.write(error+"\n");
+			response.end();
+		} else { 
+			response.writeHead(200, {"Content-Type": "image/png"});
+			response.write(file,"binary");
+			response.end();
+		}
+	});
+}
+
+function checkboxFull(response){
+	console.log("Request handler 'checkboxFull' was called.");
+	fs.readFile("./images/checkboxFull.png","binary",function(error,file){
+		if(error){
+			response.writeHead(500,{"Content-Type": "text/plain"});
+			response.write(error+"\n");
+			response.end();
+		} else { 
+			response.writeHead(200, {"Content-Type": "image/png"});
+			response.write(file,"binary");
+			response.end();
+		}
+	});
+}
+
+function checkboxEmpty(response){
+	console.log("Request handler 'show' was called.");
+	fs.readFile("./images/checkboxEmpty.png","binary",function(error,file){
+		if(error){
+			response.writeHead(500,{"Content-Type": "text/plain"});
+			response.write(error+"\n");
+			response.end();
+		} else { 
+			response.writeHead(200, {"Content-Type": "image/png"});
+			response.write(file,"binary");
+			response.end();
+		}
+	});
+}
+	
+exports.start = start;
+exports.upload = upload;
+exports.show = show;
+exports.checkboxEmpty = checkboxEmpty;
+exports.checkboxFull = checkboxFull;
